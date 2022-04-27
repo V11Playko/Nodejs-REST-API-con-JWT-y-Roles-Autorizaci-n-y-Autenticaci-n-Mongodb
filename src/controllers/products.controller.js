ab371bbfc9b957fddb9c159fea99efb73286d0d1
V11Playko <heinnervega20@gmail.com>
2022-04-27 07:17:30 -0500

    Aplicando el controlador a las rutas

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,20 @@
+
+export const createProduct = (req, res) => {
+
+}
+
+export const getProducts = (req, res) => {
+    res.json('Get Product');
+}
+
+export const getProductById = (req, res) => {
+    
+}
+
+export const updateProductById = (req, res) => {
+    
+}
+
+export const deleteProductById = (req, res) => {
+    
+}
