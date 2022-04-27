dce4eb7f0979f89ad70f666cff0bcbedf3aba5f0
V11Playko <heinnervega20@gmail.com>
2022-04-27 08:24:26 -0500

    Agregando modelos

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,20 +1,15 @@
+import Product from "../models/Product";
 
 export const createProduct = (req, res) => {
-
-}
+  res.json("Creating product");
+};
 
 export const getProducts = (req, res) => {
-    res.json('Get Product');
-}
+  res.json("Get Product");
+};
 
-export const getProductById = (req, res) => {
-    
-}
+export const getProductById = (req, res) => {};
 
-export const updateProductById = (req, res) => {
-    
-}
+export const updateProductById = (req, res) => {};
 
-export const deleteProductById = (req, res) => {
-    
-}
+export const deleteProductById = (req, res) => {};
