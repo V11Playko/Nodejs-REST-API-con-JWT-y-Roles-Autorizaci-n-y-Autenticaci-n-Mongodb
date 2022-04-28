c4f700b93a0ff5f57224a192a9690f501ec6d18e
V11Playko <heinnervega20@gmail.com>
2022-04-28 07:23:53 -0500

    Se hacen los roles basicos

---
+++ src/libs/initialSetup.js
@@ -0,0 +1,43 @@
+import Role from "../models/Role";
+import User from "../models/User";
+
+import bcrypt from "bcryptjs";
+
+export const createRoles = async () => {
+  try {
+    // Count Documents
+    const count = await Role.estimatedDocumentCount();
+
+    // check for existing roles
+    if (count > 0) return;
+
+    // Create default Roles
+    const values = await Promise.all([
+      new Role({ name: "user" }).save(),
+      new Role({ name: "moderator" }).save(),
+      new Role({ name: "admin" }).save(),
+    ]);
+
+    console.log(values);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createAdmin = async () => {
+  // check for an existing admin user
+  const user = await User.findOne({ email: "admin@localhost" });
+  // get roles _id
+  const roles = await Role.find({ name: { $in: ["admin", "moderator"] } });
+
+  if (!user) {
+    // create a new admin user
+    await User.create({
+      username: "admin",
+      email: "admin@localhost",
+      password: await bcrypt.hash("admin", 10),
+      roles: roles.map((role) => role._id),
+    });
+    console.log("Admin User Created!");
+  }
+};
